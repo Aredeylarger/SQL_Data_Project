9b74ab1dec3112466808f1b68b734f07d8fd73e0
Aredeylarger <opeyemiadelaja@gmail.com>
2025-08-16 23:34:23 +0100

    first commit

---
+++ sql_course.session.sql
@@ -0,0 +1,26 @@
+SELECT job_posted_date
+
+FROM job_postings_fact
+LIMIT 20;
+
+SELECT 
+    job_title_short AS Title,
+    job_location AS Location,
+    job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'EST' AS Posted_Date
+
+FROM 
+    job_postings_fact
+
+LIMIT 20;
+
+SELECT 
+    job_title_short AS Title,
+    job_location AS Location,
+    job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'EST' AS Posted_Date ,
+    EXTRACT(YEAR FROM job_posted_date) AS Year_Posted,
+    EXTRACT(MONTH FROM job_posted_date) AS Month_Posted
+
+FROM
+    job_postings_fact
+
+LIMIT 20;
